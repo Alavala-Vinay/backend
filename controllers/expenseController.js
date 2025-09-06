df6932f3339940451cc9e35003774444509a8c85
Alavala-Vinay <142196470+Alavala-Vinay@users.noreply.github.com>
2025-09-06 20:32:30 +0530

    Update expenseController.js

--- controllers/expenseController.js
+++ controllers/expenseController.js
@@ -8,7 +8,7 @@ exports.addExpense = async (req, res) => {
   try {
     const { icon, description, category, amount } = req.body;
 
-    if (!icon || !description || !category || !amount) {
+    if (!icon || !category || !amount || !date) {
       return res.status(400).json({ success: false, message: "All fields are required" });
     }
 
